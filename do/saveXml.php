3f4c0c2532843f5b021fda28016f9d0302fbb4f4
Aleksandar Kolundzija <akolundzija@gmail.com>
2010-04-25 09:04:37 -0400

    php script for creating an XML file

---
+++ do/saveXml.php
@@ -0,0 +1,23 @@
+<?
+/**
+ * Saves POST input as an XML file and returns a JSON response
+ */
+$xmlString;
+
+if (isset($_POST['xmlString'])){
+	$filename  = $_POST['xmlFilename'];
+	$xmlString = stripslashes($_POST['xmlString']);
+	
+	$newFile = "_data/".$filename.".edit.xml";
+	
+	//write new data to the file, along with the old data 
+	$handle = fopen("../".$newFile, "w"); 
+	if (fwrite($handle, $xmlString) === false) { 
+		echo "{error:\"Couldn't write to file.\"}";  
+	} 
+	else {
+		echo "{filename:\"".$newFile."\"}";
+	}
+	fclose($handle); 	
+}
+?>
